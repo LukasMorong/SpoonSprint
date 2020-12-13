7f0191824b2614dd91feb173ec77e228d12cb795
Lukas Morong <morong.lukas@gmail.com>
2020-12-13 01:18:54 +0100

    inst name req to false

--- Models/Recipe.js
+++ Models/Recipe.js
@@ -30,7 +30,7 @@ const RecipeSchema = new Schema({
       {
         name: {
           type: String,
-          required: true
+          required: false
         },
         desc: {
           type: String,
